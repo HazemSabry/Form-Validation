0b31203900fbe55970a11f0cb5b5a132d4d264de
HazemSabry <hazemsabry2002@gmail.com>
2024-03-24 22:32:33 +0200

    update error message color on action

--- script.js
+++ script.js
@@ -9,15 +9,18 @@ const numbers = "0123456789";
 const spacialCharacters = "!@#$%^&*()_+-={}[]:;'\"<>?/ ";
 
 const acceptableColor = "rgb(64, 130, 64)";
-
+const acceptableColor = "rgb(64, 130, 64)";
+const errorColor = "rgb(161, 83, 83)";
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const inputPasswordArray = inputPassword.value.toLowerCase().split("");
 
   if (inputName.value.trim() === "" || inputName.value.trim() === null) {
+    nameErrorMessage.style.color = errorColor;
     nameErrorMessage.innerHTML = "Name is required";
   } else if (inputName.value.trim().length <= 1) {
+    nameErrorMessage.style.color = errorColor;
     nameErrorMessage.innerHTML = "Name is too short";
   } else {
     nameErrorMessage.style.color = acceptableColor;
@@ -28,16 +31,20 @@ form.addEventListener("submit", (e) => {
     inputPassword.value.trim() === "" ||
     inputPassword.value.trim() === null
   ) {
+    passwordErrorMessage.style.color = errorColor;
     passwordErrorMessage.innerHTML = "Password is required";
   } else if (inputPassword.value.length < 8) {
+    passwordErrorMessage.style.color = errorColor;
     passwordErrorMessage.innerHTML = "Password is too short";
   } else {
     if (!inputPasswordArray.some((character) => numbers.includes(character))) {
+      passwordErrorMessage.style.color = errorColor;
       passwordErrorMessage.innerHTML =
         "Password is week it should contain numerical";
     } else if (
       !inputPasswordArray.some((character) => alphabetic.includes(character))
     ) {
+      passwordErrorMessage.style.color = errorColor;
       passwordErrorMessage.innerHTML =
         "Password is week it should contain alphabetic";
     } else if (
@@ -45,6 +52,7 @@ form.addEventListener("submit", (e) => {
         spacialCharacters.includes(character)
       )
     ) {
+      passwordErrorMessage.style.color = errorColor;
       passwordErrorMessage.innerHTML =
         "Password is week it should contain spacial Character";
     } else {
